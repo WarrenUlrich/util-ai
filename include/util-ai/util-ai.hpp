1c00a88e0bc6f38528826df1b5284ed32882df0e
Warren Ulrich <warren.ulrich@outlook.com>
2024-03-02 06:10:52 -0800

    fixed naive implementation of decision

--- include/util-ai/util-ai.hpp
+++ include/util-ai/util-ai.hpp
@@ -12,15 +12,15 @@ concept action = requires(T t, WorldState ws) {
 
 template <typename WorldState> class base_action {
 public:
-  virtual double utility(const WorldState &ws) = 0;
+  virtual double utility(WorldState &ws) = 0;
 
   virtual void execute(WorldState &ws) = 0;
 
   virtual ~base_action() = default;
 };
 
-template <typename WorldState,
-          action<WorldState> Action = base_action<WorldState>>
+template <typename WorldState, action<WorldState> Action =
+                                   base_action<WorldState>>
 class agent {
 public:
   constexpr agent() = default;
@@ -30,12 +30,15 @@ public:
   }
 
   void decide_and_act(WorldState &ws) {
-    std::sort(_actions.begin(), _actions.end(),
-              [&ws](const auto &a, const auto &b) {
-                return a->utility(ws) > b->utility(ws);
-              });
+    auto max_it = std::max_element(
+        _actions.begin(), _actions.end(),
+        [&ws](const auto &a, const auto &b) {
+          return a->utility(ws) < b->utility(ws);
+        });
 
-    _actions.front()->execute(ws);
+    if (max_it != _actions.end()) {
+      (*max_it)->execute(ws);
+    }
   }
 
 private:
